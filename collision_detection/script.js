c4e8df081cbe83143d8a8b877f4de16e17893858
tommy secker <tommysecker@outlook.co.nz>
2019-05-21 15:35:21 +1200

    week 11 work

---
+++ collision_detection/script.js
@@ -0,0 +1,11 @@
+/*eslint-env browser*/
+/*eslint no-console:0*/
+/*
+
+Begin by following the steps in the workshop notes.
+
+Next, add the following features:
+- the gap goes red when the puck is over it
+- the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
+
+*/
