ca4781aec81d58117843f7cecec5026098e8545f
tommy secker <tommysecker@outlook.co.nz>
2019-05-21 16:34:44 +1200

    Update script.js

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -9,3 +9,57 @@ Next, add the following features:
 - the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
 
 */
+
+var puck = document.getElementById('puck');
+var puck_x = 210;
+var puck_y = 210;
+var speed = 50;
+
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+
+function logCoordinates() {
+    console.log('x=' + puck_x + 'y=' + puck_y); 
+}
+
+function checkCollision() {
+    // has the puck passed the edges of the hole
+    if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250) {
+        
+    console.log('AAAAAARGH');
+        
+function changeBackground(color) {
+    
+   if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250) 
+   body.style.background = "red";
+}
+    
+    }
+}
+
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+    puck.style.left = puck_x + 'px';
+    logCoordinates();
+    checkCollision();
+});
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+    puck.style.left = puck_x + 'px';
+    logCoordinates();
+     checkCollision();
+});
+
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+    puck.style.top = puck_y + 'px';
+    logCoordinates();
+     checkCollision();
+});
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+    puck.style.top = puck_y + 'px';
+    logCoordinates();
+     checkCollision();
+});
